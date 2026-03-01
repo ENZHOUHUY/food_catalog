f690dab13bd6dec526644cd0ee761bd841d62225
ENZHOUHUY <1123150029@global.ac.id>
2026-03-01 12:15:22 +0700

    membuat constructor cart provider

--- lib/features/cart/presentation/providers/cart_provider.dart
+++ lib/features/cart/presentation/providers/cart_provider.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:food_catalog/features/cart/domain/repositories/cart_repository.dart';
 
 class CartProvider extends ChangeNotifier {
+  final CartRepository _repository;
+
+  CartProvider({required CartRepository repository}) : _repository = repository;
+
   
 } 
