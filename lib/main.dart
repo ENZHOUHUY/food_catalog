afa4d9cdddf16b0be312210765379e41dd1d6cbe
ENZHOUHUY <1123150029@global.ac.id>
2026-03-01 11:40:36 +0700

    menambahkan theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:food_catalog/core/routes/app_router.dart';
+import 'package:food_catalog/core/theme/app_theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,6 +15,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       initialRoute: AppRouter.catalog,
       routes: AppRouter.routes,
+      theme: AppTheme.lightTheme
     );
   }
 }
