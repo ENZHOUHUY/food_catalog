2c5cd9165289e5681bd532fd4b0fcb70c2c355d6
ENZHOUHUY <1123150029@global.ac.id>
2026-03-01 11:39:57 +0700

    menambahkan initial route dan routes di main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_catalog/core/routes/app_router.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,8 +12,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: ,
-      routes: ,
+      initialRoute: AppRouter.catalog,
+      routes: AppRouter.routes,
     );
   }
 }
