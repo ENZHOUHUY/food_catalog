4969d7465811dc05ba04ceaf99bef29767adf57b
ENZHOUHUY <1123150029@global.ac.id>
2026-03-01 11:39:08 +0700

    membuat routing ke catalog page dan Cart page

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:food_catalog/features/cart/pages/cart_page.dart';
+import 'package:food_catalog/features/cart/pages/catalog_page.dart';
 
 class AppRouter {
   static const String catalog = '/';
   static const String cart = '/cart';
 
   static Map<String, WidgetBuilder> get routes => {
-    catalog: (context) => 
+    catalog: (context) => CatalogPage(),
+    cart: (context) => CartPage(),
   };
 }
