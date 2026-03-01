33116ee7dbe3b74cfdba25a34b9c8884eed595c5
ENZHOUHUY <1123150029@global.ac.id>
2026-03-01 12:19:09 +0700

    membuat notify listener pada tiap tiap function

--- lib/features/cart/presentation/providers/cart_provider.dart
+++ lib/features/cart/presentation/providers/cart_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_catalog/features/cart/domain/entities/product.dart';
 import 'package:food_catalog/features/cart/domain/repositories/cart_repository.dart';
 
 class CartProvider extends ChangeNotifier {
@@ -6,5 +7,17 @@ class CartProvider extends ChangeNotifier {
 
   CartProvider({required CartRepository repository}) : _repository = repository;
 
-  
+  List<Product> get items => _repository.getCartItems();
+
+  void addItem(Product product) {
+    _repository.addItem(product);
+    notifyListeners();
+  }
+
+  void removeAll(){
+    _repository.removeAll();
+    notifyListeners();
+  }
+
+  bool isItemInCart(String productId) => _repository.isItemInCart(productId);
 } 
