968cc8007a70a8c7ec18b4a14854672413acb541
ENZHOUHUY <1123150029@global.ac.id>
2026-03-01 12:20:20 +0700

    memindahkan file file

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:food_catalog/features/cart/pages/cart_page.dart';
-import 'package:food_catalog/features/cart/pages/catalog_page.dart';
+import 'package:food_catalog/features/cart/presentation/pages/cart_page.dart';
+import 'package:food_catalog/features/cart/presentation/pages/catalog_page.dart';
 
 class AppRouter {
   static const String catalog = '/';
