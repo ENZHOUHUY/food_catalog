6875ed1d9b2ad4b359e20e70d46740afd3357a48
ENZHOUHUY <1123150029@global.ac.id>
2026-03-01 11:38:12 +0700

    membuat stateless CartPage

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -1,3 +1,10 @@
+import 'package:flutter/material.dart';
+
 class AppRouter {
-  
+  static const String catalog = '/';
+  static const String cart = '/cart';
+
+  static Map<String, WidgetBuilder> get routes => {
+    catalog: (context) => 
+  };
 }
